30aa304a7aa36e43975250cd96806a2995913935
1danil10 <danilmatvienko@gmail.com>
2021-11-21 22:39:11 +0300

    refactor: clock solution

--- clock/clock.js
+++ clock/clock.js
@@ -6,32 +6,44 @@
 export class Clock {
   static #MINUTES_IN_HOUR = 60;
   static #HOURS_IN_DAY = 24;
+  static #MINUTES_IN_DAY = Clock.#HOURS_IN_DAY * Clock.#MINUTES_IN_HOUR;
   #minutes;
   constructor(hours, minutes = 0) {
-    this.#minutes = hours * Clock.#MINUTES_IN_HOUR + minutes;
+    this.#minutes = this.#setMinutes(hours * Clock.#MINUTES_IN_HOUR + minutes);
   }
+
+  #setMinutes(value) {
+    return value < 0
+      ? (value % Clock.#MINUTES_IN_DAY) + Clock.#MINUTES_IN_DAY
+      : value;
+  }
+
   get hours() {
-    const hours =
-      Math.floor(this.#minutes / Clock.#MINUTES_IN_HOUR) % Clock.#HOURS_IN_DAY;
-    return hours >= 0 ? hours : hours + Clock.#HOURS_IN_DAY;
+    return (
+      Math.floor(this.#minutes / Clock.#MINUTES_IN_HOUR) % Clock.#HOURS_IN_DAY
+    );
   }
+
   get minutes() {
-    const minutes = this.#minutes % Clock.#MINUTES_IN_HOUR;
-    return minutes >= 0 ? minutes : minutes + Clock.#MINUTES_IN_HOUR;
+    return this.#minutes % Clock.#MINUTES_IN_HOUR;
   }
+
   toString() {
     return `${String(this.hours).padStart(2, 0)}:${String(
       this.minutes,
     ).padStart(2, 0)}`;
   }
+
   plus(value) {
-    this.#minutes += value;
+    this.#minutes = this.#setMinutes(this.#minutes + value);
     return this;
   }
+
   minus(value) {
-    this.#minutes -= value;
+    this.#minutes = this.#setMinutes(this.#minutes - value);
     return this;
   }
+
   equals(anotherClock) {
     return this.toString() === anotherClock.toString();
   }
