038589c3640c1c9bed71008e06a0f297d7b27e02
1danil10 <danilmatvienko@gmail.com>
2022-08-20 03:17:29 +0300

    fix: update bob solution

--- bob/bob.js
+++ bob/bob.js
@@ -2,62 +2,43 @@
 // This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-
-export const hey = (message) => {
-  const trimmedMsg = message.trim();
-  if (trimmedMsg.length === 0) {
-    return 'Fine. Be that way!';
-  }
-
-  const isYelling = (() => {
-    const re = /^(?=[A-Z])[^a-z]*$/g;
-    return trimmedMsg.length > 1 && re.test(trimmedMsg);
-  })();
-  console.log(isYelling);
-  const isQuestion = trimmedMsg[trimmedMsg.length - 1] === '?';
-  if (isQuestion) {
-    if (isYelling) {
-      return "Calm down, I know what I'm doing!";
-    } else {
-      return 'Sure.';
-    }
-  } else {
-    if (isYelling) {
-      return 'Whoa, chill out!';
-    } else {
-      return 'Whatever.';
-    }
-  }
+const isYelling = (str) => {
+  return str.toUpperCase() === str;
 };
 
-console.log(hey('ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!'));
-console.log(hey('4?'));
-console.log(hey('1, 2, 3 GO!'));
-// console.log(hey('Okay if like my  spacebar  quite a bit?   '));
+const isQuestion = (str) => {
+  return str.lastIndexOf('?') === str.length - 1;
+};
 
-// export const hey = (message) => {
-//   const trimmedMsg = message.trim();
-//   if (trimmedMsg.length === 0) {
-//     return 'Fine. Be that way!';
-//   }
+const isLetters = (str) => {
+  return /[a-z]/i.test(str);
+};
 
-//   const isYelling = (() => {
-//     const re = /^(?=[A-Z])[^a-z]*$/g;
-//     return trimmedMsg.length > 1 && re.test(trimmedMsg);
-//   })();
-//   console.log(isYelling);
-//   const isQuestion = trimmedMsg[trimmedMsg.length - 1] === '?';
-//   if (isQuestion) {
-//     if (isYelling) {
-//       return "Calm down, I know what I'm doing!";
-//     } else {
-//       return 'Sure.';
-//     }
-//   } else {
-//     if (isYelling) {
-//       return 'Whoa, chill out!';
-//     } else {
-//       return 'Whatever.';
-//     }
-//   }
-// };
+const getMessageInfo = (msg) => {
+  return {
+    hasLetters: isLetters(msg),
+    isMessageQuestion: isQuestion(msg),
+    isMessageYelling: isYelling(msg),
+  };
+};
+
+export const hey = (message) => {
+  let answer = null;
+  const trimmedMsg = message.trim();
+  const { hasLetters, isMessageQuestion, isMessageYelling } =
+    getMessageInfo(trimmedMsg);
+
+  if (!trimmedMsg) {
+    answer = 'Fine. Be that way!';
+  } else if (hasLetters && isMessageQuestion && isMessageYelling) {
+    answer = "Calm down, I know what I'm doing!";
+  } else if (isMessageQuestion) {
+    answer = 'Sure.';
+  } else if (hasLetters && isMessageYelling) {
+    answer = 'Whoa, chill out!';
+  } else {
+    answer = 'Whatever.';
+  }
+
+  return answer;
+};
